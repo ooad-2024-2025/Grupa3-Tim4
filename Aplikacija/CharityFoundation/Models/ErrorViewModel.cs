ca24f60d2f7ba3f7d2ff64a316f0f2cc409c44a6
Amar Husika <amar54k@gmail.com>
2025-05-10 08:47:35 +0200

    asp.net create project

---
+++ Aplikacija/CharityFoundation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CharityFoundation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
